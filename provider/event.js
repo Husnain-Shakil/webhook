892cdf6fe73f8e0b43eaecc3ada2adde0d5cf981
Husnain-Shakil <husnainshakil123@gmail.com>
2026-06-14 19:51:27 +0500

    Initialize consumer and provider

---
+++ provider/event.js
@@ -0,0 +1,17 @@
+import events from "events";
+const eventEmitter = new events.EventEmitter();
+
+const webhookEventHandler = (payload) => {
+  console.log("I hear a webhook!", payload);
+};
+
+eventEmitter.on("event", webhookEventHandler);
+
+eventEmitter.emit("webhook");
+
+const runWebHook = (payload) => {
+  eventEmitter.emit("webhook", payload);
+};
+
+export default runWebHook;
+// module.exports = { runWebHook };
---
+++ provider/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import runWebHook from "./event.js";
+const app = express();
+const port = 3100;
+
+app.get("/", (req, res) => {
+  res.send("Provider work!");
+});
+
+app.get("/webhook", (req, res) => {
+  runWebHook("test");
+  res.json("webhook provider work!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
